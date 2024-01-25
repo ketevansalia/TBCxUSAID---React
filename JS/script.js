1c31bedc24dc7b6b27128ca6817f81c3ec1839b3
ketevansalia <ketevansalia@gmail.com>
2024-01-26 01:49:30 +0400

    Add styles and JS file

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,79 @@
+// SCROLL NAVBAR
+document.addEventListener('scroll', () => {
+  const header = document.querySelector('.header');
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+// PARTNERS SLIDER
+
+const slides = document.querySelectorAll('.slide');
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener('DOMContentLoaded', initializeSlider);
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add('displaySlide');
+    intervalId = setInterval(nextSlide, 5000);
+  }
+}
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach(slide => {
+    slide.classList.remove('displaySlide');
+  });
+  slides[slideIndex].classList.add('displaySlide');
+}
+
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
+
+// FAQ
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('active');
+  });
+});
+
+// CURRENT YEAR
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
+
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
+}
+checkFlexGap();
